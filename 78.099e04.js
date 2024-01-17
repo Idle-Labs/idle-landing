d537a337c3bb8881056519d6fca6fe42b874b9f7
Samster91 <samster91>
2024-01-17 16:59:41 +0100

    Updates

---
+++ 78.099e04.js
@@ -0,0 +1,1 @@
+(self.webpackChunkidlefinance=self.webpackChunkidlefinance||[]).push([[78],{59078:(e,s,t)=>{"use strict";const n=t(70655);t(26024),t(34023);const c=n.__importDefault(t(99700));class a extends c.default{constructor(){super(...arguments),this._sectionTypes=[]}async setupPageAsync(){await super.setupPageAsync()}get sectionTypes(){return this._sectionTypes}}a.RunPage(a)}}]);
---
+++ terms-of-service.5e8c93.js
@@ -0,0 +1,1 @@
+(self.webpackChunkidlefinance=self.webpackChunkidlefinance||[]).push([[209],{},e=>{"use strict";e.O(0,[380,24,700,23,78,594,429],(()=>(59078,e(e.s=59078)))),e.O()}]);
